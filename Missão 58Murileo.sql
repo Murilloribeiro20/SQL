192443e89e17b8f53d02d3c72445dce18ee099fc
Murillo <m7olivzxr@gmail.com>
2026-04-22 11:43:02 -0300

    Add files via upload

---
+++ Missão 58Murileo.sql
@@ -0,0 +1,7 @@
+INSERT into funcionario(id_funcionario, nome, cargo, salario, fk_id_projeto, sobrenome, data_nascimento, endereco_completo)
+VALUES(1, 'neymar', 'gerente', 1500.00, 3,'da silva santos junior', '05/02/1992',  'Rua Dom Luis de Bragança, 412, São Paulo - CEP 04050-060'),
+      (2, 'Frederico', 'chutador de roteador e agiota', 2000.00, 3, 'souza martins', '04/05/1945', 'Rua Vitória, 200, Jockey de Itaparica - CEP 22267-769'),
+      (3, 'azulino', 'jogador de videogame', 400.00, 1,'Borges','20/04/1889','R.I, 54, Jardim de alah, Cariacica - CEP 29142-549'),
+      (4, 'luiz miguel', 'profissional do free fire', 2.00,1,'Miguel', '30/02/1738', 'beco da facada', '13', 'Vila Velha - CEP 23241-134'),
+      (5, 'eduardo'
+      
